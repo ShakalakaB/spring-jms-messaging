4ab4549b543a4458f463bce8ceb07c304fc33348
ShakalakaB <yunjie989@gmail.com>
2021-06-06 15:40:16 +0800

    Add message pojo

---
+++ src/main/java/aldora/spring/jms/model/HelloWorldMessage.java
@@ -0,0 +1,20 @@
+package aldora.spring.jms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+    static final long serialVersionUID = -5678474607275643006L;
+
+    private UUID id;
+    private String message;
+}
