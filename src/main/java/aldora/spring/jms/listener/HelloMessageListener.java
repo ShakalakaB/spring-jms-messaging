7ca3d727588e6398ef5f6bfc537354cf338e7c77
ShakalakaB <yunjie989@gmail.com>
2021-06-12 15:47:37 +0800

    Add spring message type example

--- src/main/java/aldora/spring/jms/listener/HelloMessageListener.java
+++ src/main/java/aldora/spring/jms/listener/HelloMessageListener.java
@@ -10,6 +10,7 @@ import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import javax.jms.Destination;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import java.util.UUID;
@@ -21,7 +22,7 @@ public class HelloMessageListener {
 
     @JmsListener(destination = JmsConfig.MY_QUEUE)
     public void listen(@Payload HelloWorldMessage helloWorldMessage,
-                       @Headers MessageHeaders headers, Message message) {
+                       @Headers MessageHeaders headers) {
 //        System.out.println("I got a message");
 //        System.out.println(helloWorldMessage);
 
@@ -30,14 +31,18 @@ public class HelloMessageListener {
 
     @JmsListener(destination = JmsConfig.MY_SEND_RCV_QUEUE)
     public void listenAndReply(@Payload HelloWorldMessage helloWorldMessage,
-                       @Headers MessageHeaders headers, Message message) throws JMSException {
+                               @Headers MessageHeaders headers, Message message,
+                               org.springframework.messaging.Message springMessage) throws JMSException {
         HelloWorldMessage payloadMsg = HelloWorldMessage
                 .builder()
                 .id(UUID.randomUUID())
                 .message("I hear you!!")
                 .build();
 
-        jmsTemplate.convertAndSend(message.getJMSReplyTo(), payloadMsg);
+//        jmsTemplate.convertAndSend(message.getJMSReplyTo(), payloadMsg);
+
+        //example to use Spring Message type
+        jmsTemplate.convertAndSend((Destination) springMessage.getHeaders().get("jms_replyTo"), "Got it through spring message");
 
     }
 }
